0cf8a1bb40faaf16c68dd69b3529f5cd855d77c2
Navas . M <navasmhd79@gmail.com>
2024-12-21 17:00:31 +0530

    slider added

---
+++ script.js
@@ -0,0 +1,8 @@
+//       Initialize Swiper
+
+const swiper = new Swiper(".slider-container", {
+    effect: "fade",
+    // other effects :- coverflow, slide , fade , cube, flip
+    speed:1300,
+    autoplay: {delay: 4000}
+});
