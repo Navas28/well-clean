e75e5aa579a882ee4719f65447dc4cdb0fcf9a3b
Navas . M <navasmhd79@gmail.com>
2024-12-31 18:10:12 +0530

    review added

--- script.js
+++ script.js
@@ -2,7 +2,32 @@
 
 const swiper = new Swiper(".slider-container", {
     effect: "fade",
-    // other effects :- coverflow, slide , fade , cube, flip
+    // other effects :- cover flow, slide , fade , cube, flip
     speed:1300,
     autoplay: {delay: 4000}
+});
+
+
+//    testimonial slider
+
+const testimonialSwiper = new Swiper('.testimonial-slider', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
 });
